e6abbfa873071393c400e5ff56d3d924516af55a
Bix <lasdo2@outlook.com>
2025-12-01 12:33:06 -0300

    main.c arrumado

--- src/main.c
+++ src/main.c
@@ -17,8 +17,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "args.h"
-#include "colors.h"
 
 
 
@@ -26,21 +24,7 @@ int
 main (int argc, char* argv[])
 {
 
-    /* Read command line options */
-    options_t options;
-    options_parser(argc, argv, &options);
 
-
-#ifdef DEBUG
-    fprintf(stdout, BLUE "Command line options:\n" NO_COLOR);
-    fprintf(stdout, BROWN "help: %d\n" NO_COLOR, options.help);
-    fprintf(stdout, BROWN "version: %d\n" NO_COLOR, options.version);
-    fprintf(stdout, BROWN "use colors: %d\n" NO_COLOR, options.use_colors);
-    fprintf(stdout, BROWN "filename: %s\n" NO_COLOR, options.file_name);
-#endif
-
-
-    /* Do your magic here :) */
 
 
     return EXIT_SUCCESS;
