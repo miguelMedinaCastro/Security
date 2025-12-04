44945ac124fdbd27b384bda813799e21528c16ea
miguelmedinacastro <miguelmedinadecastro@gmail.com>
2025-12-04 09:22:34 -0300

    .h ajustado

--- include/rsa.h
+++ include/rsa.h
@@ -9,12 +9,12 @@
 RSA *load_public_key(const char *pub_key);
 RSA *load_private_key(const char *priv_key);
 void error();
-void encrypted_file(const char *input, const char *output);
-void decrypt_file(const char *input, const char *output);
+void encrypted_file(const char *input, const char *output, const char *pub_key);
+void decrypt_file(const char *input, const char *output, const char *priv_key);
 double rsa_time_encrypt(const char *input, const char *output, const char *pub_key);
 double rsa_time_decrypt(const char *input, const char *output, const char *priv_key);
-double time_now();
 long size(const char *input);
+double time_now();
 
 
 #endif
